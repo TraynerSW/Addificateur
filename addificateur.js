fba7d9cd101f4ca0f85d30faf57d269ce0ec6be7
Mateo Vazquez <matou.vazquez@gmail.com>
2025-11-16 00:43:55 +0100

    Added x10 and x100 buttons, new font, updated button shapes and fixes

--- addificateur.js
+++ addificateur.js
@@ -1,26 +1,99 @@
 let count = 0;
 
-let x1 = document.getElementById("x1");
 let res = document.getElementById("res");
+let x1 = document.getElementById("x1");
+let x10 = document.getElementById("x10");
+let x100 = document.getElementById("x100");
+let x1000 = document.getElementById("x10³");
+let x10e4 = document.getElementById("x10⁴");
 
-res.innerHTML = count;		// Pour afficher le résultat dès 0
 
-let wait = null;		// Temps d'attente avant le mode automatique
-let autoX1 = null;		// Incrémente automatiquement si appui prolongé
+res.innerHTML = count;		// To show the result starting from 0
+
+let wait = null;			// Time before the "autoX" mode
+let auto1 = null;			// Adds automaticaly if a button is long pressed
+let auto10 = null;	
+let auto100 = null;
+let auto1000 = null;		
 
 // x1 \\
+
 x1.addEventListener("pointerdown", () => {
 	count += 1;
 	res.innerHTML = count;
-	wait = setTimeout(() => {
-		autoX1 = setInterval(() => {
-								count += 1; 	
-								res.innerHTML = count;
-								}, 100);
+
+	if (count >= 100) {							// If the score reaches 100,
+		x10.style.visibility = "visible";		// show the button x10.
+	}
+
+	wait = setTimeout(() => {					// If pressed, wait 500ms,
+		auto1 = setInterval(() => {				// and then activate "auto" mode.
+			count += 1; 	
+			res.innerHTML = count;
+			if (count >= 100) {
+				x10.style.visibility = "visible";
+			}
+		}, 100);
 	}, 500);
 });
 
 x1.addEventListener("pointerup", () => {
+	clearTimeout(auto1);
 	clearInterval(wait);
-	clearTimeout(autoX1);
 });
+
+
+// x10 \\
+x10.addEventListener("pointerdown", () => {
+	count += 1;
+	res.innerHTML = count;
+
+	if (count >= 1000) {
+		x100.style.visibility = "visible";
+	}
+
+	wait = setTimeout(() => {
+		auto10 = setInterval(() => {
+									count += 1; 	
+									res.innerHTML = count;
+									if (count >= 1000) {
+										x100.style.visibility = "visible";
+									}
+								}, 10);
+	}, 500);
+});
+
+x10.addEventListener("pointerup", () => {
+	clearTimeout(auto10);
+	clearInterval(wait);
+});
+
+
+// x100 \\
+x100.addEventListener("pointerdown", () => {
+	count += 1;
+	res.innerHTML = count;
+
+	if (count >= 10000) {
+	 	x1000.style.visibility = "visible";
+	}
+
+	wait = setTimeout(() => {
+		auto10 = setInterval(() => {
+									count += 1; 	
+									res.innerHTML = count;
+									if (count >= 10000) {
+										x1000.style.visibility = "visible";
+									}
+								}, 1);
+	}, 500);
+});
+
+x100.addEventListener("pointerup", () => {
+	clearTimeout(auto10);
+	clearInterval(wait);
+});
+
+
+
+// Auto click \\
